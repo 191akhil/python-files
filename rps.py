20ba289cd18cf383b10bb2cc88163d45314da5e7
191akhil <43162737+191akhil@users.noreply.github.com>
2018-10-05 16:32:00 +0530

    Add files via upload

---
+++ rps.py
@@ -0,0 +1,13 @@
+import random
+d={1:'r',2:'p',3:'s'}
+c=d[random.randint(1,3)]
+while(1):
+	p=input("enter 'r','p','s'")
+	print("computer",c)
+	if(c==p):
+		print("tie")
+	elif(c=='r'and p=='s'or c=='p'and p=='r'or c=='s'and p=='p'):
+		print("computer wins")
+	else:
+		print("player wins")
+    	
